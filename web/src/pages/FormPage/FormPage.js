be03b03325f8edf6412336d3ec3aa77f7bf91be1
kirby ellingson <hkellingson@gmail.com>
2022-07-30 10:00:12 -0500

    w/commits

--- web/src/pages/FormPage/FormPage.js
+++ web/src/pages/FormPage/FormPage.js
@@ -1,6 +1,6 @@
+import { Form, TextField, Submit } from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import { Form, TextField, Submit } from '@redwoodjs/forms'
 const FormPage = () => {
   const onSubmit = (data) => {
     // push info to DB here.
@@ -11,13 +11,16 @@ const FormPage = () => {
 
       <h1>Form | Resume</h1>
       <Form>
-      <TextField name="name" placeholder="name"/>
-      <TextField name="email" placeholder="email"/>
-      <TextField name="jobTitle" placeholder="Job Title"/>
-      <TextField name="jobExp" placeholder="Work Experience"/>
-      <TextField name="yrsInIndustry" placeholder="Total years worked in tech"/>
-      <TextField name="education" placeholder="Education" />
-      <Submit>Submit</Submit>
+        <TextField name="name" placeholder="name" />
+        <TextField name="email" placeholder="email" />
+        <TextField name="jobTitle" placeholder="Job Title" />
+        <TextField name="jobExp" placeholder="Work Experience" />
+        <TextField
+          name="yrsInIndustry"
+          placeholder="Total years worked in tech"
+        />
+        <TextField name="education" placeholder="Education" />
+        <Submit>Submit</Submit>
       </Form>
     </>
   )
