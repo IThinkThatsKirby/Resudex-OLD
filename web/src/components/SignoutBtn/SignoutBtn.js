6126360c08749bad8a1184b1dd8f46a966fd31ef
Soullessly <alexbarthel01@gmail.com>
2022-08-06 09:21:09 -0500

    Simple Styling.

--- web/src/components/SignoutBtn/SignoutBtn.js
+++ web/src/components/SignoutBtn/SignoutBtn.js
@@ -1,11 +1,12 @@
 import { useAuth } from '@redwoodjs/auth'
 import { navigate, routes } from '@redwoodjs/router'
+import 'src/index.css'
 const SignoutBtn = () => {
   const { logOut } = useAuth()
   const onClick = () => {
     logout().then(() => navigate(routes.home))
   }
-  return <button onClick={() => onClick()}>Sign Out</button>
+  return <button className="signOutBtn" onClick={() => onClick()}>Sign Out</button>
 }
 
 export default SignoutBtn
--- web/src/pages/FormPage/FormPage.js
+++ web/src/pages/FormPage/FormPage.js
@@ -1,7 +1,7 @@
 import { Form, TextField, Submit, NumberField } from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-
+import 'src/index.css'
 const FormPage = () => {
   const onSubmit = (data) => {
     // push info to DB here.
@@ -9,19 +9,19 @@ const FormPage = () => {
   return (
     <>
       <MetaTags title="Form" description="Form page" />
+      <div className="formContainer">
+      <h1 className="formTitle">Form | Resume</h1>
 
-      <h1>Form | Resume</h1>
-
-      <Form>
+      <Form className="userInputForm">
         <TextField name="name" placeholder="name" />
         <TextField name="email" placeholder="email" />
         <TextField name="jobTitle" placeholder="Job Title" />
         <TextField name="jobExp" placeholder="Work Experience" />
         <TextField name="yrsInIndustry" placeholder="Total years worked in tech"/>
         <TextField name="education" placeholder="Education" />
-        <Submit>Submit</Submit>
-
+        <Submit className="SubmitBtn">Submit</Submit>
       </Form>
+</div>
     </>
   )
 }
