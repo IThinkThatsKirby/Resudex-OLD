25872f9ace8fb77288e8d32f7513170b20232dfe
kirby ellingson <hkellingson@gmail.com>
2022-08-06 10:09:49 -0500

    ifix the schema again

--- web/src/pages/FormPage/FormPage.js
+++ web/src/pages/FormPage/FormPage.js
@@ -9,21 +9,23 @@ const FormPage = () => {
   return (
     <>
       <MetaTags title="Form" description="Form page" />
-      <main class="responsive">
-      <body class="dark">
-      <h1>Form | Resume</h1>
+      <main className="responsive">
+        <body className="dark">
+          <h1>Form | Resume</h1>
 
-      <Form>
-        <TextField name="name" placeholder="name" />
-        <TextField name="email" placeholder="email" />
-        <TextField name="jobTitle" placeholder="Job Title" />
-        <TextField name="jobExp" placeholder="Work Experience" />
-        <TextField name="yrsInIndustry" placeholder="Total years worked in tech"/>
-        <TextField name="education" placeholder="Education" />
-        <Submit>Submit</Submit>
-
-      </Form>
-      </body>
+          <Form>
+            <TextField name="name" placeholder="name" />
+            <TextField name="email" placeholder="email" />
+            <TextField name="jobTitle" placeholder="Job Title" />
+            <TextField name="jobExp" placeholder="Work Experience" />
+            <TextField
+              name="yrsInIndustry"
+              placeholder="Total years worked in tech"
+            />
+            <TextField name="education" placeholder="Education" />
+            <Submit>Submit</Submit>
+          </Form>
+        </body>
       </main>
     </>
   )
