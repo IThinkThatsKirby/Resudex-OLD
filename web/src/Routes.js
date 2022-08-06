f322b81839217ca17e320c7117b2b6c49b4f5b39
kirby ellingson <hkellingson@gmail.com>
2022-08-06 11:02:27 -0500

    useraccount handleing framework

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -15,6 +15,9 @@ import UsersLayout from 'src/layouts/UsersLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={UsersLayout}>
         <Route path="/users/new" page={UserNewUserPage} name="newUser" />
         <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
