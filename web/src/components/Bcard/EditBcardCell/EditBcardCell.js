f338e5f7f692618a8ff5a84cce960e6fa8f188a4
kirby ellingson <hkellingson@gmail.com>
2022-08-14 00:11:40 -0500

    some cleanup

--- web/src/components/Bcard/EditBcardCell/EditBcardCell.js
+++ web/src/components/Bcard/EditBcardCell/EditBcardCell.js
@@ -1,5 +1,4 @@
 import { navigate, routes } from '@redwoodjs/router'
-
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
@@ -81,7 +80,7 @@ export const Success = ({ bcard }) => {
         </h2>
       </header>
 
-      <div className="rw-segment-main">
+      <div className="">
         <BcardForm
           bcard={bcard}
           onSave={onSave}
